a9fd2f5e8666e532408b3c9f8c93bfea67b19e87
IceJim <jimliu@chcgp.com>
2014-03-12 17:53:38 -0700

    Update fileUpload.js

--- fileUpload.js
+++ fileUpload.js
@@ -1,4 +1,4 @@
-function iframeUpload(generalId){
+function iframeUpload(generalId){	
 	window.onbeforeunload = function() {
 	    return "Data you have entered may not be saved if you leave the page, are you sure?";
 	};
@@ -9,42 +9,42 @@ function iframeUpload(generalId){
 	
 	var form = $('#upload_form'+generalId)[0];
 	var iframe = document.createElement("iframe");
-    iframe.setAttribute("id", "upload_iframe"+generalId);
-    iframe.setAttribute("name", "upload_iframe");
-    iframe.setAttribute("width", "0");
-    iframe.setAttribute("height", "0");
-    iframe.setAttribute("border", "0");
-    iframe.setAttribute("style", "width: 0; height: 0; border: none;");
-    
-    // Add to document...
-    document.body.appendChild(iframe);
-    
-    var eventHandler = function () {
-    	 
-        if (iframe.detachEvent) iframe.detachEvent("onload", eventHandler);
-        else iframe.removeEventListener("load", eventHandler, false);
-
-        // Message from server...
-        if (iframe.contentDocument) {
-            content = iframe.contentDocument.body.innerHTML;
-        } else if (iframe.contentWindow) {
-            content = iframe.contentWindow.document.body.innerHTML;
-        } else if (iframe.document) {
-            content = iframe.document.body.innerHTML;
-        }
-
-    }
-    
-    if (iframe.addEventListener) iframe.addEventListener("load", eventHandler, true);
-    if (iframe.attachEvent) iframe.attachEvent("onload", eventHandler);
- 
-    // Set properties of form...
-    form.setAttribute("target", "upload_iframe");
- 
-    // Submit the form...
-    form.submit();
-    
-    window.onbeforeunload = null;
+	iframe.setAttribute("id", "upload_iframe"+generalId);
+	iframe.setAttribute("name", "upload_iframe");
+	iframe.setAttribute("width", "0");
+	iframe.setAttribute("height", "0");
+	iframe.setAttribute("border", "0");
+	iframe.setAttribute("style", "width: 0; height: 0; border: none;");
+	
+	// Add to document...
+	document.body.appendChild(iframe);
+	
+	var eventHandler = function () {
+	     
+	if (iframe.detachEvent) iframe.detachEvent("onload", eventHandler);
+	else iframe.removeEventListener("load", eventHandler, false);
+	
+	// Message from server...
+	if (iframe.contentDocument) {
+	    content = iframe.contentDocument.body.innerHTML;
+	} else if (iframe.contentWindow) {
+	    content = iframe.contentWindow.document.body.innerHTML;
+	} else if (iframe.document) {
+	    content = iframe.document.body.innerHTML;
+	}
+	
+	}
+	
+	if (iframe.addEventListener) iframe.addEventListener("load", eventHandler, true);
+	if (iframe.attachEvent) iframe.attachEvent("onload", eventHandler);
+	
+	// Set properties of form...
+	form.setAttribute("target", "upload_iframe");
+	
+	// Submit the form...
+	form.submit();
+	
+	window.onbeforeunload = null;
 }
 
 function ajaxUpload(generalId){
@@ -58,32 +58,32 @@ function ajaxUpload(generalId){
 	fupb.setAttribute("id", "fupb"+generalId);
 	fupb.max = 1000;
 	$('#bar'+generalId).append(fupb);
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('progress', function(e) {
-        var done = e.position || e.loaded, total = e.totalSize || e.total;
-        console.log('xhr progress: ' + (Math.floor(done/total*1000)/10) + '%');
-        var pct = done / total;
-        fupb.value = Math.min(1000, pct * 1000) || 1000;
-    }, false);
-    if ( xhr.upload ) {
-        xhr.upload.onprogress = function(e) {
-            var done = e.position || e.loaded, total = e.totalSize || e.total;
-            console.log('xhr.upload progress: ' + done + ' / ' + total + ' = ' + (Math.floor(done/total*1000)/10) + '%');
-            var pct = done / total;
-            fupb.value = Math.min(1000, pct * 1000) || 1000;
-        };
-    }
-    xhr.onreadystatechange = function(e) {
-        if ( 4 == this.readyState ) {
-            console.log(['xhr upload complete', e]);
-            var result = (xhr.status == 200 ? "success" : "failure");
-           //...result handler
-        }
-    };
-    xhr.open('post', your_url, true);
-    xhr.send(formDate);
-    
-    window.onbeforeunload = null;
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('progress', function(e) {
+	var done = e.position || e.loaded, total = e.totalSize || e.total;
+	console.log('xhr progress: ' + (Math.floor(done/total*1000)/10) + '%');
+	var pct = done / total;
+	fupb.value = Math.min(1000, pct * 1000) || 1000;
+	}, false);
+	if ( xhr.upload ) {
+	xhr.upload.onprogress = function(e) {
+	    var done = e.position || e.loaded, total = e.totalSize || e.total;
+	    console.log('xhr.upload progress: ' + done + ' / ' + total + ' = ' + (Math.floor(done/total*1000)/10) + '%');
+	    var pct = done / total;
+	    fupb.value = Math.min(1000, pct * 1000) || 1000;
+	};
+	}
+	xhr.onreadystatechange = function(e) {
+	if ( 4 == this.readyState ) {
+	    console.log(['xhr upload complete', e]);
+	    var result = (xhr.status == 200 ? "success" : "failure");
+	   //...result handler
+	}
+	};
+	xhr.open('post', your_url, true);
+	xhr.send(formDate);
+	
+	window.onbeforeunload = null;
 }
 
 function upload(generalId){
